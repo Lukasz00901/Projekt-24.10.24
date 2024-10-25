1b201dd713b15a1ab8017357f9f9686e271114b7
Lukas00901 A7D3 <Lukas009012@outlook.de>
2024-10-25 11:35:12 +0200

    25.10.24

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -7,8 +7,8 @@ function Footer() {
      <div className={styles.footer}>
       
        <footer>
-       <p>&copy; 2024 Stefan, Markus, Lukas, Jeffrey, Muditha und Ivonne. Alle Rechte vorbehalten!</p>
-      <a className='link' href="/impressum">Impressum</a> | <a href="/Datenschutz">Datenschutz</a>   
+       <p>&copy; 2024 Lukas, Jeffrey, Ivonne, Muditha, Stefan & Masko. Alle Rechte vorbehalten</p>
+      <a href="/impressum">Impressum</a> | <a href="/Datenschutz">Datenschutz</a>   
           {/*  <Link to="/impressum">Impressum</Link> | <Link to="/Datenschutz">Datenschutz</Link> */}
        </footer> 
         </div>
